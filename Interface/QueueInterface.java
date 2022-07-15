b1aa9a5d88066cb00725ba8d54ec4b32e8084681
dlawnsdh <77203609+dlawnsdh@users.noreply.github.com>
2022-07-15 19:15:14 +0900

    Delete Interface directory

--- Interface/QueueInterface.java
+++
@@ -1,9 +0,0 @@
-package queue;
-
-public interface QueueInterface<E> {
-	public void enqueue(E x);
-	public E dequeue();
-	public E front();
-	public boolean isEmpty();
-	public void dequeueAll();
-}
